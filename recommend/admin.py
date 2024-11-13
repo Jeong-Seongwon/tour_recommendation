d5433ba4f9a9e6e198736affb3e5a7f36e853849
Jeong-Seongwon <bohomo1234@gmail.com>
2024-11-13 10:17:54 +0900

    admin 페이지 수정

--- recommend/admin.py
+++ recommend/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from recommend.models import Travel, Consume, Visit
 
-admin.site.register(Travel)
-admin.site.register(Consume)
-admin.site.register(Visit)
+
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+    list_display = ('visit_name', 'address', 'photos')
+
+@admin.register(Travel)
+class TravelAdmin(admin.ModelAdmin):
+    list_display = ('traveler', 'travel_name', 'start_date', 'end_date', 'movement_name', 'companion_num', 'relationship')
+    readonly_fields = ('visits',)
+
+
+@admin.register(Consume)
+class ConsumeAdmin(admin.ModelAdmin):
+    list_display = ('category', 'consume_name', 'payment_amount', 'travel', 'details')
